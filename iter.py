aff9e1fd8869a7d011661da678fcfa97d3a6e51e
Mayank <mynksrjpt@gmail.com>
2020-12-01 09:50:32 +0530

    final

--- iter.py
+++ iter.py
@@ -2,14 +2,35 @@ from itertools import combinations
 N = int(input())
 list1 = input().split()
 K = int(input())
-print(list1)
+# print(list1)
 
 comb = combinations(list1,K)
 
-for i in list(comb) :  
-    print(i)
+count = 0
+total = 0 
+
+for i in list(comb) :
+    total += 1
+    bEqual = False 
+    for e in i :
+        if 'a' == e :
+            bEqual = True
+            break
+    if bEqual :
+            count = count + 1
+    else :
+            count = count + 0   
+
+    # print(i)
+
+# print(total)
+# print(count)
+
+prob = count/total
+print(prob)
 
 
-for e in list(comb) :
-    print(e)
+
+
+
      
